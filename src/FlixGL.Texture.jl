2fc6d98068f893efee4fe3b3c96803103054f1c9
Kiruse <neurokiruse@outlook.com>
2020-05-02 17:22:45 +0200

    Integrated grayscale colors with texture

--- src/FlixGL.Texture.jl
+++ src/FlixGL.Texture.jl
@@ -27,6 +27,8 @@ import ..LowLevel
 componentlayout(img::AbstractImage)    = componentlayout(extract_color_type(img))
 componentlayout(::Type{<:OpaqueColor}) = LowLevel.RGBLayout
 componentlayout(::Type{<:Color})       = LowLevel.RGBALayout
+componentlayout(::Type{<:GrayscaleColor}) = LowLevel.RGLayout
+componentlayout(::Type{<:OpaqueGrayscaleColor}) = LowLevel.RLayout
 
 componenttype(img::AbstractImage) = componenttype(extract_color_type(img))
 componenttype(::Type{<:AbstractColor{T}}) where T = T
